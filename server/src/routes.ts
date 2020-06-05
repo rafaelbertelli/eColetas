7506f7972fa4df8dd2db26da5e3f65cfd5f21b4b
Rafael Borges <rafael.borges@luizalabs.com>
2020-06-05 17:04:32 -0300

    creates route to list points

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -13,7 +13,8 @@ routes.get("/", (req, res) => {
 
 routes.get("/items", itemsController.index);
 
-routes.get("/points/:id", pointsController.show);
 routes.post("/points", pointsController.create);
+routes.get("/points/", pointsController.index);
+routes.get("/points/:id", pointsController.show);
 
 export default routes;
